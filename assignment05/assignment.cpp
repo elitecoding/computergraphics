a0cbebfaba0bbd084f3000be6fef54ae0169f7fb
Julian Arnold <julian.arnold@rwth-aachen.de>
2013-11-19 23:20:37 +0100

    added matrikelnumbers, names (somehow not merged)

--- assignment05/assignment.cpp
+++ assignment05/assignment.cpp
@@ -14,10 +14,10 @@ extern unsigned char g_bunnyColor[];
 // add your team members names and matrikel numbers here:
 void printStudents()
 {
-	cout << "Julian Arnold, 293370" << endl;
-	cout << "Student Name 1, matrikel number 1" << endl;
-	cout << "Student Name 2, matrikel number 2" << endl;
-	cout << "Student Name 3, matrikel number 3" << endl;
+	cout << "Julian Arnold,  293370" << endl;
+	cout << "Marius Lipka,   334336" << endl;
+	cout << "Florian George, 234236" << endl;
+	cout << "Houman Biglari, 279942" << endl;
 }
 
 glm::mat4 g_ModelViewMatrix;
